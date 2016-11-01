22992ffb4877c56cfb3a3d00daea1d0872c3c480
mrogach2350 <m.rogach@gmail.com>
2016-11-01 15:30:35 -0700

    adds logic to players.html

---
+++ client/players/Players.js
@@ -0,0 +1,12 @@
+Meteor.subscribe('players');
+
+Template.Players.helpers({
+  players: ()=> {
+    return Players.find({});
+  }
+});
+Template.Players.events({
+  'click .delete'() {
+    Players.remove(this._id);
+  }
+});
