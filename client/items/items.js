1659294dad272f8f6bb4bc3bf798ab2d733c072f
mrogach2350 <m.rogach@gmail.com>
2016-10-31 17:14:46 -0700

    adds event logic

--- client/items/items.js
+++ client/items/items.js
@@ -2,6 +2,15 @@ Meteor.subscribe('items');
 
 Template.Items.helpers({
   items: ()=> {
-    return Items.find({owner: this.userId});
+    return Items.find({inGroupStash: false});
+  }
+});
+
+Template.Items.events({
+  'click .toggle-group'() {
+    Meteor.call('toggleGroupItem', this._id, this.inGroupStash);
+  },
+  'click .delete'() {
+    Items.remove(this._id);
   }
 });
