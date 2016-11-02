5ed1835c5324a7b7b1f661dd189044113eb467c5
mrogach2350 <m.rogach@gmail.com>
2016-11-01 20:46:48 -0700

    added change owner method

--- client/items/GroupItems.js
+++ client/items/GroupItems.js
@@ -8,6 +8,14 @@ Template.GroupItems.helpers({
 Template.GroupItems.events({
   'click .toggle-group'() {
     Meteor.call('toggleGroupItem', this._id, this.inGroupStash);
+
+    var current = FlowRouter.current();
+    if (current.params.characterName){
+      newOwner = current.params.characterName;
+    } else {
+      newOwner = "DM";
+    }
+    Meteor.call('togglePlayerItem', this._id, newOwner );
   },
   'click .delete'() {
     Items.remove(this._id);
