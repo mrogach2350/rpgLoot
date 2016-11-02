2e655d6b9e53f5bea3336661d2ab4185d4426800
mrogach2350 <m.rogach@gmail.com>
2016-11-02 11:14:37 -0700

    updates helper functions for players items

--- client/items/PlayerItems.js
+++ client/items/PlayerItems.js
@@ -1,23 +1,17 @@
 Meteor.subscribe('items');
 
 Template.PlayerItems.helpers({
-<<<<<<< HEAD
   currentPlayer: ()=> {
     var current = FlowRouter.current();
     return current.params.characterName;
   },
 
-=======
->>>>>>> 1c91285ae9576a91c7850a75bed46cfea7788c4b
   items: ()=> {
     var current = FlowRouter.current();
     return Items.find({owner: current.params.characterName, inGroupStash: false });
   }
 });
-<<<<<<< HEAD
 
-=======
->>>>>>> 1c91285ae9576a91c7850a75bed46cfea7788c4b
 Template.PlayerItems.events({
   'click .toggle-group'() {
     Meteor.call('toggleGroupItem', this._id, this.inGroupStash);
