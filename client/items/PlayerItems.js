827b22b62a77a493d5096fc9bb9e9f7c35fc1894
mrogach2350 <m.rogach@gmail.com>
2016-11-01 20:47:19 -0700

    adds FlowRouter.current method

--- client/items/PlayerItems.js
+++ client/items/PlayerItems.js
@@ -2,13 +2,15 @@ Meteor.subscribe('items');
 
 Template.PlayerItems.helpers({
   items: ()=> {
-    return Items.find({inGroupStash: false});
+    var current = FlowRouter.current();
+    return Items.find({owner: current.params.characterName, inGroupStash: false });
   }
 });
 Template.PlayerItems.events({
   'click .toggle-group'() {
     Meteor.call('toggleGroupItem', this._id, this.inGroupStash);
   },
+
   'click .delete'() {
     Items.remove(this._id);
   }
