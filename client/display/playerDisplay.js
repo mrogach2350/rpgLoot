a7ae41f5c1ea7fdf2e9e28554ef276f0ce91767b
mrogach2350 <m.rogach@gmail.com>
2017-02-15 15:48:44 -0800

    adds dice rolling

--- client/display/playerDisplay.js
+++ client/display/playerDisplay.js
@@ -7,8 +7,29 @@ Template.playerDisplay.helpers({
   }
 });
 
+roll = function(n){
+ var num = (Math.floor(Math.random()*1000))%n;
+ if(num !== 0){
+   return num;
+ } else {
+   return n;
+ }
+}
+
 Template.playerDisplay.events({
-  'submit .new-item-form'(){
-    console.log(diceRoller.roll('4d20'));
+  'click #d6'(){
+    Display.insert({message: ' rolled a d6 for '+ roll(6), owner:FlowRouter.current().params.characterName})
+  },
+  'click #d8'(){
+    Display.insert({message: ' rolled a d8 for '+ roll(8), owner:FlowRouter.current().params.characterName})
+  },
+  'click #d10'(){
+    Display.insert({message: ' rolled a d10 for '+ roll(10), owner:FlowRouter.current().params.characterName})
+  },
+  'click #d20'(){
+    Display.insert({message: ' rolled a d20 for '+ roll(20), owner:FlowRouter.current().params.characterName})
+  },
+  'click #clearLog'(){
+    Meteor.call('deleteAllPosts');
   }
 })
