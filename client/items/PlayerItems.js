1de1cb51dd03fb7caae04940b097de5d743f786b
mrogach2350 <m.rogach@gmail.com>
2017-02-15 14:30:34 -0800

    updates methods to save to display collection

--- client/items/PlayerItems.js
+++ client/items/PlayerItems.js
@@ -14,7 +14,11 @@ Template.PlayerItems.helpers({
 
 Template.PlayerItems.events({
   'click .toggle-group'() {
+    console.log(this);
     Meteor.call('toggleGroupItem', this._id, this.inGroupStash);
+    if (!this.inGroupStash){
+      Display.insert({message:' gave up ' + this.name, owner:this.owner});
+    };
   },
 
   'click .delete'() {
